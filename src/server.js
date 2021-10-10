a13aff114daaa9d97a12d4f595caa07fec421c11
Diogenes Yazan <76189529+DiogenesYazan@users.noreply.github.com>
2021-10-10 13:23:53 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const nunjucks = require("nunjucks");
+const compression = require("compression");
+const zlib = require("zlib");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(cors());
+app.disable("x-powered-by");
+app.use(compression({ level: 9 }));
+app.use(express.static(path.join(__dirname, "..", "public")));
+nunjucks.configure(path.join(__dirname, "views"), {
+  express: app,
+  noCache: true,
+});
+app.use(routes)
+
+
+app.listen(process.env.PORT || 3000);
